b373c4c6ef07c09c0b1cd418c091fb2d246622ed
Vladislav <vladislavhacker111@gmail.com>
2019-10-25 14:29:03 +0300

    binaryMSD

--- sorts/heap_sort.cpp
+++ sorts/heap_sort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using std::cin;
 using std::cout;
+
 template <typename MyType>
 void shiftDown( MyType* a, int i, int n) {
     int firstChild = 2 * i + 1;
@@ -12,32 +13,34 @@ void shiftDown( MyType* a, int i, int n) {
     if (secondChild < n && a[max] < a[secondChild]) {
         max = secondChild;
     }
-    if (max != i) {
+    if (max != i) { //нам не нужно менять элемент так как он уже максимальный.
         MyType t = a[i];
         a[i] = a[max];
         a[max] = t;
-        shiftDown(a, max, n);
+        shiftDown(a, max, n); //востанавливакм свойство кучи
     }
 }
+
 template <typename  MyType>
 void buildHeap(MyType* a, int n) {
     for (int i = n/2; i >= 0; i--){
-       shiftDown(a,i,n);
+       shiftDown(a, i, n); //востанавливакм свойство кучи
     }
 }
 
 template <typename  MyType>
 void heapSort( MyType* a, int n) {
-    buildHeap(a, n);
+    buildHeap(a, n); //строим кучу О(n)
     int m = n;
     for (int i = n - 1; i > 0; i--){
         MyType t = a[i];
         a[i] = a[0];
-        a[0] = t;
-        m--;
-        shiftDown(a,0,m);
+        a[0] = t; //меняем местами последний элемент с корнем кучи
+        m--; // на i-m месте стоит нужный элемент, он не нужен в куче
+        shiftDown(a,0,m); //востановление свойства кучи
     }
 }
+
 int main() {
     int n;
     cin>>n;
