0767b36287d34e672e4996d451dfdeac5f262c1d
vhdev <30373406+VladislavHacker@users.noreply.github.com>
2020-03-12 16:30:03 +0300

    Update k-stat.cpp

--- sorts/k-stat.cpp
+++ sorts/k-stat.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include  <algorithm>
-#include <cstdlib>
 
 using std::cin;
 using std::cout;
@@ -8,48 +7,53 @@ using std::endl;
 using std::swap;
 using std::rand;
 
-int partition(int *a, int l, int r) {
-   int index = rand() % (r - l + 1) + l;
-   int pivot = a[index];
-   swap(a[l],a[index]);
-   int i = r;
-   for (int j = r; j > l; j--) {
-       if (a[j] > pivot) {
-           swap(a[i],a[j]);
-           i--;
-       }
-   }
+template <typename T>
+int partition(T *a, int l, int r) {
+    int mid = l + (r - l) / 2;
+
+    if (a[l] > a[r])
+        swap(a[l], a[r]);
+    if (a[mid] > a[r])
+        swap(a[mid],a[r]);
+    if (a[l] < a[mid])
+        swap(a[l],a[mid]);
+
+    int pivot = a[l];
+
+    int i = r;
+    for (int j = r; j > l; j--) {
+        if (a[j] > pivot) {
+            swap(a[i],a[j]);
+            i--;
+        }
+    }
     swap(a[i],a[l]);
+
     return i;
 }
 
-void qsort(int *a, int l, int r) {
-    if (l < r) {
-        int p = partition(a, l, r);
-        qsort(a, l,p - 1);
-        qsort(a, p + 1, r);
+template <typename T>
+T kstat(T *a, int l, int r,int& k) {
+    int p;
+    while (l < r){
+         p = partition(a, l, r);
+         if (p == k)
+             return a[k];
+         if (k < p)
+             r = p - 1;
+         else l = p + 1;
     }
-}
-
-int kstat(int *a, int l, int r, int& k) {
-    if (l < r) {
-        int p = partition(a, l, r);
-        if (p == k)
-            return a[k];
-        if (k < p)
-            return kstat(a,l,p - 1,k);
-        else return kstat(a,p + 1, r, k);
-    } else return a[k];
+    return a[k];
 }
 
 int main() {
     int n, k;
-    cin>>n>>k;
+    cin >> n >> k;
     int* a = new int[n];
     for (int i = 0; i < n; i++) {
-        cin>>a[i];
+        cin >> a[i];
     }
-    cout<<kstat(a, 0, n-1, k);
-    delete []a;
+    cout << kstat(a,0,n-1,k);
+    delete[] a;
     return 0;
 }
